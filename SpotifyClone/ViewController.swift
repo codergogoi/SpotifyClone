27b14f24039e8e18a47597d26b411615d15c5703
devakbar <34447154+devakbar@users.noreply.github.com>
2019-12-30 21:58:27 +0530

    Initial Code Structure

---
+++ SpotifyClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyClone
+//
+//  Created by JAYANTA GOGOI on 12/30/19.
+//  Copyright © 2019 JAYANTA GOGOI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpotifyCloneTests/SpotifyCloneTests.swift
@@ -0,0 +1,34 @@
+//
+//  SpotifyCloneTests.swift
+//  SpotifyCloneTests
+//
+//  Created by JAYANTA GOGOI on 12/30/19.
+//  Copyright © 2019 JAYANTA GOGOI. All rights reserved.
+//
+
+import XCTest
+@testable import SpotifyClone
+
+class SpotifyCloneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
